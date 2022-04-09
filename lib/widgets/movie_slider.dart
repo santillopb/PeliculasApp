99633413206b0677e93c064ba419f6c151e0a920
LAPTOP-B9KF4GKF\santi <santiago.llop@xarxatecactiva.com>
2022-04-09 13:12:25 +0200

    Finalizar el MovieSlider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -7,8 +7,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 260,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -17,6 +16,8 @@ class MovieSlider extends StatelessWidget {
             child: Text('Populares', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
           ),
           
+          SizedBox(height: 5),
+
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
@@ -37,8 +38,33 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          SizedBox(height: 5),
+
+          Text(
+            'Star Wars: El retorno del nuevo Jedi silvestre de Montecristo',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
