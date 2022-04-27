1229869541fa3a27a30935ce170000155b7c7f5b
LAPTOP-B9KF4GKF\santi <santiago.llop@xarxatecactiva.com>
2022-04-27 17:44:30 +0200

    Peliculas populares

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -12,11 +12,13 @@ class MoviesProvider extends ChangeNotifier {
   String _language = 'es-ES';
 
   List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
 
   MoviesProvider() {
     print('Movies provider inicializado');
 
     this.getOnDisplayMovies();
+    this.getPopularMovies();
   }
   getOnDisplayMovies() async{
     var url = Uri.https(_baseUrl, '3/movie/now_playing', {
@@ -32,6 +34,20 @@ class MoviesProvider extends ChangeNotifier {
     this.onDisplayMovies = nowPlayingResponse.results;
 
     notifyListeners();
+
   }
+  getPopularMovies() async {
+      var url = Uri.https(_baseUrl, '3/movie/popular', {
+        'api_key': _apiKey,
+        'language': _language,
+        'page': '1'
+      });
+
+      final response = await http.get(url);
+      final popularResponse = PopularResponse.fromJson(response.body);
+
+      popularMovies = [...popularMovies, ...popularResponse.results];
+      notifyListeners();
+    }
 }
 
